40b2632e1f9a9579403067bfcdd567f5449f0e0f
xiezheng <510819030@qq.com>
2019-08-02 19:09:05 +0800

    增加了utils文件

---
+++ utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "utils.h"
+void function1()
+{
+    printf("call function1()\n");
+}
---
+++ utils.h
@@ -0,0 +1,5 @@
+#ifndef __UTILS_H
+#define __UTILS_H
+void function1(void);
+
+#endif
